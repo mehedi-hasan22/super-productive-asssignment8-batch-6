66009bd879678b3a6f97ff2ebca2cff4b91e9789
mehedi-hasan22 <mehedi30554@gmail.com>
2022-11-10 02:09:24 +0600

    added navbar

---
+++ src/Components/Activity/Activity.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Activity = () => {
+    return (
+        <div>
+            <h2>Hello from activity</h2>
+        </div>
+    );
+};
+
+export default Activity;
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Main.css'
+import Activity from '../Activity/Activity';
+
+const Main = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light navbar-dark bg-primary">
+                <div className="container-fluid text-white">
+                    <a className="navbar-brand fs-4 m-3" href="/#">Super Productive</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav fs-5 ms-auto mb-2 mb-lg-0 text-white">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/#">History</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="/#">Login</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <h1>Hello from main</h1>
+            <Activity></Activity>
+        </div>
+    );
+};
+
+export default Main;
